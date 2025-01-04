506d3e3619e370c32fa04443cb8ec52eb5bb2d92
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-01-04 18:50:16 +0000

    add size_t

--- include/stdint.h
+++ include/stdint.h
@@ -12,6 +12,8 @@ typedef unsigned long int uint32_t;
 typedef signed long long int int64_t;
 typedef unsigned long long int uint64_t;
 
+typedef unsigned int size_t;
+
 typedef uint8_t bool;
 #define true 1
 #define false 0
