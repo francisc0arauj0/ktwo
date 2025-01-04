88e596475bb4c073538d62ce6f63ece83570f848
Francisco Araújo <franciscocoelhoaraujo492000@gmail.com>
2025-01-04 17:10:28 +0000

    update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,3 +1,4 @@
+#include "gdt.h"
 #include "vga.h"
 
 void kmain(void);
@@ -5,4 +6,6 @@ void kmain(void);
 void kmain(void) {
 	print("ktwo kernel\n");
 	print("\n[VGA] Finished");
+	initGDT();
+	print("\n[GDT] Finished");
 }
