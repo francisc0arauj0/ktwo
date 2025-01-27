53a9c6b013f9ccf17ca5eedc346381e8a2cc6e9b
Francisco <franciscocoelhoaraujo492000@gmail.com>
2025-01-27 08:08:20 +0000

    update inPortB

--- arch/x86/kernel/io.c
+++ arch/x86/kernel/io.c
@@ -7,6 +7,6 @@ void outPortB(uint16_t port, uint8_t value) {
 
 char inPortB(uint16_t port) {
 	char rv;
-	__asm__ volatile("inb %1, %0": "=a"(rv):"dN"(port));
+	__asm__ volatile("inb %1, %0" : "=a"(rv) : "dN"(port));
 	return rv;
 }
